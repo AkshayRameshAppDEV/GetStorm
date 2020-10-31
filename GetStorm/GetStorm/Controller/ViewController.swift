1804a74e76bdffb10366776f0a77c6dbbab44572
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-10-31 13:36:39 -0500

    Get pictures only list from directory

--- GetStorm/GetStorm/Controller/ViewController.swift
+++ GetStorm/GetStorm/Controller/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fileManager = FileManager.default
+        let projectPath = Bundle.main.resourcePath!
+        do {
+            let contents = try fileManager.contentsOfDirectory(atPath: projectPath)
+            for content in contents {
+                if content.hasPrefix("nssl") {
+                    print(content)
+                }
+            }
+        } catch {
+            print(error)
+        }
+        print(projectPath)
     }
 
 
